2b20fe487f229363f2ed0a624fe1ab917ab351be
alexispace <papadi166@gmail.com>
2024-11-03 00:58:52 +0100

    Mouse movement with AI added to the project

--- Source/Aura/Private/Interaction/EnemyInterface.cpp
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -4,3 +4,12 @@
 #include "Interaction/EnemyInterface.h"
 
 // Add default functionality here for any IEnemyInterface functions that are not pure virtual.
+void IEnemyInterface::HighlightActor()
+{
+
+	
+}
+
+void IEnemyInterface::UnHighlightActor()
+{
+}
