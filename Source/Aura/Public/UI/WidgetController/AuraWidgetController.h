7e28d3f0331bfc2503cadfd1d97f1bcb26021c6f
alexispace <papadi166@gmail.com>
2024-11-03 23:27:38 +0100

    widget and wiget controller filled with code

--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "UObject/NoExportTypes.h"
 #include "AuraWidgetController.generated.h"
 
@@ -13,5 +14,18 @@ UCLASS()
 class AURA_API UAuraWidgetController : public UObject
 {
 	GENERATED_BODY()
+
+	protected:
+	UPROPERTY(BlueprintReadOnly, Category="WidgetContoller")
+	TObjectPtr<APlayerController> PlayerController;
+
+	UPROPERTY(BlueprintReadOnly, Category="WidgetContoller")
+	TObjectPtr<APlayerState> PlayerState;
+
+	UPROPERTY(BlueprintReadOnly, Category="WidgetContoller")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category="WidgetContoller")
+	TObjectPtr<UAttributeSet> AttributeSet;
 	
 };
